671f2c35ae5482dad2d4bcd7219fe527d4cb29db
lucifer <ridhobgs2010@gmail.com>
2025-06-22 00:15:12 +0700

    Fix .gitignore and untrack ignored files

--- app/src/main/assets/script.js
+++
@@ -1,9 +0,0 @@
-function runCommand() {
-    const cmd = document.getElementById('command').value;
-    if (!cmd) {
-        alert("Please enter a command");
-        return;
-    }
-    const result = window.Android.runSuCommand(cmd);
-    document.getElementById('output').textContent = result;
-}
